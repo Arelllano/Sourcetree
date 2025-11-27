2146777cf129ed09439147279f7ae73dcee467e2
A <arellanttony@gmail.com>
2025-11-26 22:52:53 -0600

    Agregando 5 variables mas

---
+++ Ejercicio 1.py
@@ -0,0 +1,23 @@
+# %%
+# Prueba de Python 
+# Se añadio la variable c
+a = 10
+b = 30
+c = a + b
+print('Resultado = ', c)
+
+Se agregan 3 variables d, e y f como resultado de las variables d y e
+
+d = 5
+e = 25
+f = d + e
+
+print('Resultado = ', f)
+
+# Se deberan conciderar 5 variables mas pra el futuro 
+
+m = 6
+n = 7
+o = 55
+p = 98
+q = 23
